787820f11c8b54595cd219c86456baecd8747d1b
Mark Hope <mark.hope@live.co.uk>
2012-12-09 21:42:19 +0000

    Populate git repository

---
+++ OliviaTrelloControl/Gesture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OliviaTrelloControl
+{
+    enum Gesture
+    {
+        HandUp,
+        HandDown,
+        None,
+        NotInteracting
+    }
+}
